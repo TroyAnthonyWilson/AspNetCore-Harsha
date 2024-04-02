38b5cdf2053fd39d5fa99e6ca6eba7bcc4a10be9
Troy Wilson <102485470+TroyAnthonyWilson@users.noreply.github.com>
2024-04-02 14:19:09 -0400

    Update Program.cs

--- 04. Routing/01. Intro to Routing/RoutingExample/Program.cs
+++ 04. Routing/01. Intro to Routing/RoutingExample/Program.cs
@@ -1,13 +1,38 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    Endpoint endPoint = context.GetEndpoint();
+    await next(context);
+});
+
 //enable routing
 app.UseRouting();
+
+app.Use(async (context, next) =>
+{
+    Endpoint endPoint = context.GetEndpoint();
+    await next(context);
+});
 
 //creating endpoints
 app.UseEndpoints(endpoints =>
 {
-  //add your endpoints here
+    //add your endpoints here
+    endpoints.MapGet("map1", async (context) =>
+    {
+        await context.Response.WriteAsync("In Map 1");
+    });
 
+    endpoints.MapPost("map2", async (context) =>
+    {
+        await context.Response.WriteAsync("In Map 2");
+    });
+});
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync($"Home {context.Request.Path}");
 });
 app.Run();
